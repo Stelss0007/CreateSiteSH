a5da0c89b771f45627e5e39b2f2f3908f9c6767c
Ruslan <stelss1986@gmail.com>
2017-06-02 18:03:07 +0300

    Updated main command

--- testing.site.visual/index.php
+++
@@ -1,9 +0,0 @@
-
-              <html>
-                <head></head>
-                <body>
-                  <h2>It's work!</h2>
-                  <?php phpinfo() ?>
-                </body>
-              </html>
-              
